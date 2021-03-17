fe005f49ef91be8ec8a001bd0da9a388c812a08e
AndreiHogea <andreihga@gmail.com>
2021-03-18 01:11:31 +0200

    - bugged out quick sort

--- src/main/java/org/App.java
+++ src/main/java/org/App.java
@@ -4,10 +4,11 @@ import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) {
-        int[] unsortedArray = {10, 6, 8, 5, 7, 3, 4, 23, 6};
-        System.out.println("Bubble sort: " + Arrays.toString(BubbleSort.bubbleSort(unsortedArray))); // sorted ascending BubbleSort
+        int[] unsortedArray = {10, 6, 8, 5, 7};
+//        System.out.println("Bubble sort: " + Arrays.toString(BubbleSort.bubbleSort(unsortedArray))); // sorted ascending BubbleSort
 //        System.out.println(Arrays.toString(BubbleSort.descendingBubbleSort(unsortedArray))); // sorted descending BubbleSort
 
-        System.out.println("Merge sort " + Arrays.toString(MergeSort.mergeSort(unsortedArray)));
+//        System.out.println("Merge sort " + Arrays.toString(MergeSort.mergeSort(unsortedArray)));
+        System.out.println("Quick sort " + Arrays.toString(QuickSort.quickSort(unsortedArray,0,5)));
     }
 }
--- src/main/java/org/QuickSort.java
+++ src/main/java/org/QuickSort.java
@@ -1,4 +1,36 @@
 package org;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class QuickSort {
+
+    public static int[] quickSort(int[] unsortedArray, int start, int end){
+        if(start<end){
+            int partitionIndex = partition(unsortedArray,start,end);
+            quickSort(unsortedArray,start,partitionIndex-1);
+            quickSort(unsortedArray,start,partitionIndex+1);
+        }
+        return unsortedArray;
+    }
+
+    public static int partition(int[] unsortedArray, int startIndex, int endIndex) {
+        Random random = new Random();
+        int pivot = random.nextInt(unsortedArray.length);
+        int partitionIndex = startIndex;
+        for (int i = startIndex; i < endIndex; i++) {
+            if (unsortedArray[i] < unsortedArray[pivot]) {
+                swapElements(unsortedArray,partitionIndex, i);
+                partitionIndex++;
+            }
+        }
+        swapElements(unsortedArray,partitionIndex,pivot);
+        return partitionIndex;
+    }
+
+    public static void swapElements(int[] array, int a, int b) {
+        int aux = array[a];
+        array[a] = array[b];
+        array[b] = aux;
+    }
 }
