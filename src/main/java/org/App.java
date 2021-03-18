f83b572a2119175b2e074c2c787a571796bb7cab
hogik <ci5Quasa>
2021-03-18 10:25:43 +0200

    - solved bugs for quickSort

--- src/main/java/org/App.java
+++ src/main/java/org/App.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) {
-        int[] unsortedArray = {10, 6, 8, 5, 7};
+        int[] unsortedArray = {10, 6, 8, 5, 7, 4, 1, 2};
 //        System.out.println("Bubble sort: " + Arrays.toString(BubbleSort.bubbleSort(unsortedArray))); // sorted ascending BubbleSort
 //        System.out.println(Arrays.toString(BubbleSort.descendingBubbleSort(unsortedArray))); // sorted descending BubbleSort
 
 //        System.out.println("Merge sort " + Arrays.toString(MergeSort.mergeSort(unsortedArray)));
-        System.out.println("Quick sort " + Arrays.toString(QuickSort.quickSort(unsortedArray,0,5)));
+        System.out.println("Quick sort " + Arrays.toString(QuickSort.quickSort(unsortedArray, 0, unsortedArray.length-1)));
     }
 }
--- src/main/java/org/QuickSort.java
+++ src/main/java/org/QuickSort.java
@@ -5,27 +5,28 @@ import java.util.Random;
 
 public class QuickSort {
 
-    public static int[] quickSort(int[] unsortedArray, int start, int end){
-        if(start<end){
-            int partitionIndex = partition(unsortedArray,start,end);
-            quickSort(unsortedArray,start,partitionIndex-1);
-            quickSort(unsortedArray,start,partitionIndex+1);
+    public static int[] quickSort(int[] unsortedArray, int start, int end) {
+        if (start < end) {
+            int partitionIndex = partition(unsortedArray, start, end);
+            quickSort(unsortedArray, start, partitionIndex - 1);
+            quickSort(unsortedArray, partitionIndex + 1, end);
         }
         return unsortedArray;
     }
 
     public static int partition(int[] unsortedArray, int startIndex, int endIndex) {
         Random random = new Random();
-        int pivot = random.nextInt(unsortedArray.length);
-        int partitionIndex = startIndex;
-        for (int i = startIndex; i < endIndex; i++) {
-            if (unsortedArray[i] < unsortedArray[pivot]) {
-                swapElements(unsortedArray,partitionIndex, i);
+        int pivot = random.nextInt((endIndex - startIndex) + 1) + startIndex;// (4-3)+1
+        swapElements(unsortedArray, startIndex, pivot);
+        int partitionIndex = startIndex + 1;
+        for (int i = partitionIndex; i <= endIndex; i++) {
+            if (unsortedArray[i] < unsortedArray[startIndex]) {
+                swapElements(unsortedArray, partitionIndex, i);
                 partitionIndex++;
             }
         }
-        swapElements(unsortedArray,partitionIndex,pivot);
-        return partitionIndex;
+        swapElements(unsortedArray, startIndex, partitionIndex - 1);
+        return partitionIndex - 1;
     }
 
     public static void swapElements(int[] array, int a, int b) {
